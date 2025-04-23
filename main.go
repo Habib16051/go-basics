49a26ca80e2ebf0be1327a6cf95c9c6e860c71fd
Habib16051 <habib16051@gmail.com>
2025-04-23 14:12:24 +0600

    we created our own custom package

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"example.com/mathlib"
+	"fmt"
+)
 
 func main() {
 
 	x := 20
 	fmt.Println("Result =", x) // Print a message
+
+	// Package scope
+	// Tries to run:
+	// go init mod example.com
+	// Now we are using our own custom package
+	fmt.Println("Our Custom Package :")
+	resultOfMultiplication := mathlib.Multiplication(10, 15)
+	fmt.Println("Result =", resultOfMultiplication)
+
 }
---
+++ mathlib/math.go
@@ -0,0 +1,6 @@
+package mathlib
+
+func Multiplication(a, b int) int {
+	res := a * b
+	return res
+}
