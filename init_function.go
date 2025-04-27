f3d76010181df73bcf66f15c5afd835e3c2da746
habib-technonext <habibur.rahman@technonext.com>
2025-04-27 10:00:18 +0600

    comment is added

--- init_function.go
+++ init_function.go
@@ -13,3 +13,7 @@ func init() {
 	a = 25
 
 }
+
+//In Go, the init function is a special function that gets called automatically when a package
+//is initialized—before the main function runs (in the main package) or before the package is used (in imported packages).
+//In Go, global variable initialization happens before the init() function is called
